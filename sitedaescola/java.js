f7a9ca495caf66496540d3121fccb477025226fe
KaykCaetano2 <127310295+kaykcaetano2@users.noreply.github.com>
2024-11-07 14:01:32 -0300

    Add files via upload

--- sitedaescola/java.js
+++ sitedaescola/java.js
@@ -1,39 +1,39 @@
 function escolha() {
     var nome = document.getElementById("nome").value;
-    var dia = prompt("Qual dia você deseja coloca-ló")
+    var dia = prompt("Qual dia você deseja coloca-ló?(de sengunda à sexta. Certifique-se que escreveu certo)")
         if(dia =="segunda"){
             document.getElementById("seg").innerHTML = `${nome}`;
             horario = 1
-            hora
+            hora()
         }
         else if(dia =="terça"){
             document.getElementById("ter").innerHTML = `${nome}`;
             horario = 2
-            hora
+            hora()
         }
         else if(dia =="quarta"){
             document.getElementById("qua").innerHTML = `${nome}`;
             horario = 3
-            hora
+            hora()
         }
         else if(dia =="quinta"){
             document.getElementById("qui").innerHTML = `${nome}`;
             horario = 4
-            hora
+            hora()
         }
         else if(dia =="sexta"){
             document.getElementById("sex").innerHTML = `${nome}`;
             horario = 5
-            hora
+            hora()
         }
         else{
             alert("Mudou de ideia, né, tudo bem")
         }
-    return horario
-    
+        hora
+    return horario   
 }
 function hora(){
-    var hora = prompt("Qual Horario é você deseja coloca-ló")
+    var hora = prompt("Qual Horario é você deseja coloca-ló(coloque Horarios como: 18:00  19:57  13:30)")
     if(horario =="1")
         document.getElementById("ho1").innerHTML = `${hora}`;
     else if(horario =="2")
