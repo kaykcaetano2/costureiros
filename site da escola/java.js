93b27829c6c7314e568bdea3b246ca92340856aa
KaykCaetano2 <127310295+kaykcaetano2@users.noreply.github.com>
2024-11-07 13:14:46 -0300

    Add files via upload

---
+++ site da escola/java.js
@@ -0,0 +1,49 @@
+function escolha() {
+    var nome = document.getElementById("nome").value;
+    var dia = prompt("Qual dia você deseja coloca-ló")
+        if(dia =="segunda"){
+            document.getElementById("seg").innerHTML = `${nome}`;
+            horario = 1
+            hora
+        }
+        else if(dia =="terça"){
+            document.getElementById("ter").innerHTML = `${nome}`;
+            horario = 2
+            hora
+        }
+        else if(dia =="quarta"){
+            document.getElementById("qua").innerHTML = `${nome}`;
+            horario = 3
+            hora
+        }
+        else if(dia =="quinta"){
+            document.getElementById("qui").innerHTML = `${nome}`;
+            horario = 4
+            hora
+        }
+        else if(dia =="sexta"){
+            document.getElementById("sex").innerHTML = `${nome}`;
+            horario = 5
+            hora
+        }
+        else{
+            alert("Mudou de ideia, né, tudo bem")
+        }
+    return horario
+    
+}
+function hora(){
+    var hora = prompt("Qual Horario é você deseja coloca-ló")
+    if(horario =="1")
+        document.getElementById("ho1").innerHTML = `${hora}`;
+    else if(horario =="2")
+        document.getElementById("ho2").innerHTML = `${hora}`;
+    else if(horario =="3")
+        document.getElementById("ho3").innerHTML = `${hora}`;
+    else if(horario =="4")
+        document.getElementById("ho4").innerHTML = `${hora}`;
+    else if(horario =="5")
+        document.getElementById("ho5").innerHTML = `${hora}`;
+    else
+        alert("Mudou de ideia, né, tudo bem")
+}
